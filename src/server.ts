9a2df3ab9309c68ae3b76588de10e772daab1aec
mahdi <mxhdiqaim@gmail.com>
2026-01-10 00:57:53 +0100

    add ESLint configuration and update package dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import "./config/instrument";
+// import "./config/instrument";
 import * as Sentry from "@sentry/node";
 import cors from "cors";
 import express from "express";
 import http from "http";
 import morgan from "morgan";
-import "./config/auth-config";
+// import "./config/auth-config";
 import path from "path";
 import configureSession from "./config/session-config";
 import routes from "./routes";
